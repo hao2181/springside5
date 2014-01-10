3982c292181f7c37ba0138312e8e34e10839eb7c
Calvin <calvin.xiao@ericsson.com>
2014-01-10 18:43:16 +0800

    @RestController

--- samples/advanced-service/src/main/java/org/springside/examples/quickservice/rest/TaskRestController.java
+++ samples/advanced-service/src/main/java/org/springside/examples/quickservice/rest/TaskRestController.java
@@ -1,18 +1,13 @@
 package org.springside.examples.quickservice.rest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TaskRestController {
-	private static Logger logger = LoggerFactory.getLogger(TaskRestController.class);
 
 	@RequestMapping("/")
-	@ResponseBody
 	public String home() {
-		return "Hello World!!";
+		return "Hello World!!!";
 	}
 }
--- samples/quick-service/src/main/java/org/springside/examples/quickservice/rest/TaskRestController.java
+++ samples/quick-service/src/main/java/org/springside/examples/quickservice/rest/TaskRestController.java
@@ -5,30 +5,22 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springside.examples.quickservice.domain.Task;
 import org.springside.examples.quickservice.rest.support.MediaTypes;
 import org.springside.examples.quickservice.service.TaskService;
 
-@Controller
-@RequestMapping(value = "/api/v1/task")
+@RestController
+@RequestMapping(value = "/task")
 public class TaskRestController {
 	private static Logger logger = LoggerFactory.getLogger(TaskRestController.class);
 
 	@Autowired
 	private TaskService taskService;
 
-	@RequestMapping("/")
-	@ResponseBody
-	public String home() {
-		return "Hello World!!";
-	}
-
 	@RequestMapping(method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
-	@ResponseBody
 	public List<Task> list() {
 		return taskService.getAllTask();
 	}
